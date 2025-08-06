0dbdcae2acbf73a97a21f842e66f6a0c431d7c28
BopheloMpopo22 <bophelompopo22@gmail.com>
2025-08-06 17:24:09 +0200

    fix: remove trailing slash from Yoco API endpoint

--- api/create-yoco-payment.js
+++ api/create-yoco-payment.js
@@ -107,9 +107,9 @@ export default async function handler(req, res) {
       "Making Yoco API call with secret key:",
       process.env.YOCO_SECRET_KEY?.substring(0, 20) + "..."
     );
-    console.log("Yoco API endpoint: https://online.yoco.com/v2/checkout/");
+    console.log("Yoco API endpoint: https://online.yoco.com/v2/checkout");
 
-    const yocoResponse = await fetch("https://online.yoco.com/v2/checkout/", {
+    const yocoResponse = await fetch("https://online.yoco.com/v2/checkout", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
